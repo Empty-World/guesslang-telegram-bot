30ed17ce7810e2224aa733314a2ea22f47c051df
AbhijithNT <abhijithnt64@gmail.com>
2021-10-30 13:17:42 +0530

    environment variables

---
+++ bot/plugins/env.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def env(name: str, terminal_action: bool = True):
+    """
+    Get environment variables or you can input your variables into the terminal.
+    """
+    if name in os.environ:
+        return os.environ[name]
+
+    try:
+        if terminal_action is True:
+            values = input(f'Enter your {name}: ')
+
+            return values
+        else:
+            return name
+    except Exception as e:
+        print(e)

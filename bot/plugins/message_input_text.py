8bdbee844f4464e6944d3161708eb9aa2fd499b1
AbhijithNT <abhijithnt64@gmail.com>
2021-10-30 15:13:11 +0530

    results

--- bot/plugins/message_input_text.py
+++ bot/plugins/message_input_text.py
@@ -5,4 +5,6 @@ def text_message(update: Update, _) -> None:
     """
     get from user text
     """
+    message_text = update.message.text
+    result = gess_language_name()
     print(update.message.text)

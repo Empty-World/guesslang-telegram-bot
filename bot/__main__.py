fa0dbf24f8f50be34cf6c129337b370e0a5f46b9
AbhijithNT <abhijithnt64@gmail.com>
2021-10-30 13:38:53 +0530

    guesslang

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,14 +1,22 @@
 from bot import BOT_TOKEN
 from .plugins.commands import start_msg
-from telegram.ext import Updater, CommandHandler
-
+from .plugins.f import text_message
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters
+)
 
 
 if __name__ == "__main__":
 
     updater = Updater(BOT_TOKEN)
+    dispatcher = updater.dispatcher
 
-    updater.dispatcher.add_handler(CommandHandler('start', start_msg))
+    dispatcher.add_handler(CommandHandler('start', start_msg))
+    dispatcher.add_handler(MessageHandler(
+        Filters.text & ~Filters.command, text_message))
 
     updater.start_polling()
     updater.idle()

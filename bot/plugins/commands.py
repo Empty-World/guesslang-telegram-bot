03fe659524054f514ed857beef027f3806b7b0fa
AbhijithNT <abhijithnt64@gmail.com>
2021-10-30 15:06:26 +0530

    -_-

--- bot/plugins/commands.py
+++
@@ -1,8 +0,0 @@
-from telegram import Update
-from telegram.ext import CommandHandler
-
-def start_msg(update: Update,_) -> None:
-    """
-    telegram  start command replay message
-    """
-    update.message.reply_text("Hello,  World")
